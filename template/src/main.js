88115e8db5a7283ccf1da54b23c7d15a46ac1051
Jee Hoon <yamoo9@naver.com>
2017-07-21 01:00:54 +0900

    ESLint 오류 발생 문제 해결 main.js

--- template/src/main.js
+++ template/src/main.js
@@ -11,6 +11,9 @@ import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 // 환경설정: false로 설정할 경우, 배포에 대한 Tip을 출력하지 않습니다.
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+// 아래 라인 주석을 지우지 마세요.
+/* eslint-disable no-new */
+
 // Vue 루트 인스턴스 생성 및 설정
 new Vue({
   el: '#app',
