8d34bf791d9bef3673d411824edeaa1896d32e08
Jee Hoon <yamoo9@naver.com>
2017-08-06 15:26:08 +0900

    sass 디렉토리 별칭 등록

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,5 +4,5 @@ gitbook build
 cd _book
 git init
 git add -A
-git commit -m '북 업데이트'
+git commit -m 'Book 업데이트'
 git push -f git@github.com:yamoo9/git-full.git master:gh-pages
--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -24,7 +24,8 @@ module.exports = {
       {{#if_eq build "standalone"}}
       'vue$': 'vue/dist/vue.esm.js',
       {{/if_eq}}
-      '@': resolve('src')
+      '@': resolve('src'),
+      'sass': resolve('src/sass'),
     }
   },
   module: {
